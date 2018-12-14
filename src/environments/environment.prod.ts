d515a4cb030e821103cdda0328a51ae4127b4b3c
lafulga <hninja049@gmail.com>
2018-12-14 10:31:00 -0500

    firts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBP4jlGegH74VTVyC6TrDjxfkzrZJEyauc",
+    authDomain: "crud-angular-e6d7b.firebaseapp.com",
+    databaseURL: "https://crud-angular-e6d7b.firebaseio.com",
+    projectId: "crud-angular-e6d7b",
+    storageBucket: "crud-angular-e6d7b.appspot.com",
+    messagingSenderId: "578987326470"
+  }  
+};
